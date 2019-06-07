41471c7bee4da5b5aefe864778048fdb341190ec
Piyush Ranjan <2017ugcs007@nitjsr.ac.in>
2019-06-07 14:39:49 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -9,7 +9,7 @@ function create(){
   mkdir $1
   cd $1
   git init
-  echo "# "$1" #">>readme.md
+  echo "# "$1" #">>README.md
   git add .
   git remote add origin "https://$username:$password@github.com/$username/$1"
   # if you have @ in the username or password then replace the above line with- git remote add origin "https://github.com/$username/$1"
