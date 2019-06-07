e6d02ac496de593c07c496e64cd8926afe3ad0d9
Piyush <2017ugcs007@nitjsr.ac.in>
2019-06-07 13:26:34 +0530

    script for automation

--- commands.sh
+++ commands.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
 function create(){
-  
+  mkdir $1
+  cd $1
+  username="username"
+  python create.py $1
+  git init
+  echo "# "$1" #">>readme.md
+  git add .
+  git remote add origin "https://github.com/$username/$1"
+  git commit -m "intital commit"
+  git push -u origin master
 }
